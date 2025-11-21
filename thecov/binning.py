355a189250c75278ea447ea728360fb7ca5e42cc
Joe Adamo <joedadamo@gmail.com>
2025-11-21 16:43:10 -0700

    Added pytests + file omitted from last commit

---
+++ thecov/binning.py
@@ -0,0 +1,217 @@
+import numpy as np
+from thecov import math
+
+class FourierBinning:
+    """A parent class to represent an observable binned in wavenumber k."""
+
+    def __init__(self):
+        self.kmin = None
+        self.kmax = None
+        self.dk = None
+        self._nmodes = None
+
+    def set_kbins(self, kmin:float, kmax:float, dk:float, nmodes=None):
+        '''This function defines the k-bins.
+
+        Parameters
+        ----------
+        kmin: float
+            The minimum value of the wavenumber k.
+        kmax: float
+            The maximum value of the wavenumber k.
+        dk: float
+            The (log) spacing between k-bins.
+        nmodes: numpy.ndarray, optional
+            The number of modes to be used in the calculation. It is an optional parameter.
+            If omitted, it is calculated from the volume of spherical shells.
+        '''
+
+        self.dk = dk
+        self.kmax = kmax
+        self.kmin = kmin
+        self._nmodes = nmodes
+
+    @property
+    def kbins(self):
+        '''Returns the total number of k-bins.
+
+        Returns
+        -------
+        int
+            The total number of k-bins.
+        '''
+
+        return len(self.kmid)
+    
+    @property
+    def is_kbins_set(self):
+        '''Check if k-bins were defined.
+
+        Returns
+        -------
+            bool, True if k-bins were defined, False otherwise.
+        '''
+        return None not in (self.dk, self.kmin, self.kmax)
+    
+    @property
+    def kavg(self):
+        '''
+        Returns the average k of the k-bins. Assumes spherical approximation to
+        integrate k-modes, which fails for small k.
+
+        Returns
+        -------
+        numpy.ndarray
+            The average k of the k-bins.
+        '''
+        return 3/4*(self.kedges[1:]**4 - self.kedges[:-1]**4)/ \
+                   (self.kedges[1:]**3 - self.kedges[:-1]**3)
+    
+    @property
+    def kfun(self):
+        '''Fundamental wavenumber of the box 2*pi/Lbox.
+
+        Returns
+        -------
+        float
+            The fundamental wavenumber of the box.
+        '''
+
+        return 2*np.pi/self.volume**(1/3)
+
+    @property
+    def kedges(self):
+        raise NotImplementedError
+
+    @property
+    def nmodes(self):
+        raise NotImplementedError
+
+    @nmodes.setter
+    def nmodes(self, nmodes):
+        '''Manually sets the number of modes per k-bin shell.
+
+        Parameters
+        -------
+        nmodes : numpy.ndarray
+            The number of modes per k-bin shell.
+        '''
+
+        self._nmodes = nmodes
+
+class LinearBinning(FourierBinning):
+    '''A class to represent an observable linearly binned in wavenumber k.
+
+    Attributes
+    ----------
+    kmin: float
+        The minimum value of the wavenumber k.
+    kmax: float
+        The maximum value of the wavenumber k.
+    dk: float
+        The spacing between k-bins.
+    '''
+
+    def __init__(self, kmin:float=None, kmax:float=None, dk:float=None) -> None:
+        super().__init__()
+        self.set_kbins(kmin, kmax, dk)
+
+    @property
+    def kmid(self):
+        '''
+        Returns the midpoints of the k-bins.
+
+        Returns
+        -------
+        numpy.ndarray
+            The midpoints of the k-bins.
+        '''
+        return np.arange(self.kmin + self.dk/2, self.kmax + self.dk/2, self.dk)
+
+    @property
+    def kedges(self):
+        '''
+        Returns the edges of the k-bins.
+
+        Returns
+        -------
+        numpy.ndarray
+            The edges of the k-bins.
+        '''
+
+        return np.arange(self.kmin, self.kmax + self.dk/2, self.dk)
+
+    @property
+    def nmodes(self):
+        '''This function calculates the number of modes per k-bin shell. If nmodes was not provided, it is
+        extimated from the volume of each shell.
+
+        Returns
+        -------
+        numpy.ndarray
+            The number of modes per k-bin shell.
+        '''
+
+        if hasattr(self, '_nmodes'):
+            return self._nmodes
+
+        return math.nmodes(self.volume, self.kedges[:-1], self.kedges[1:])
+
+
+class LogBinning(FourierBinning):
+    '''A class to represent an observable logarithmically binned in wavenumber k.
+
+    Attributes
+    ----------
+    kmin: float
+        The minimum value of the wavenumber k.
+    kmax: float
+        The maximum value of the wavenumber k.
+    dk: float
+        The logarithmic spacing between k-bins.
+    '''
+
+    def __init__(self, kmin:float=None, kmax:float=None, dk:float=None) -> None:
+        super().__init__()
+        self.set_kbins(kmin, kmax, dk)
+
+    @property
+    def kmid(self):
+        '''
+        Returns the midpoints of the k-bins.
+
+        Returns
+        -------
+        numpy.ndarray
+            The midpoints of the k-bins.
+        '''
+        return np.sqrt(self.kedges[:-1]*self.kedges[1:])
+
+    @property
+    def kedges(self):
+        '''
+        Returns the edges of the k-bins.
+
+        Returns
+        -------
+        numpy.ndarray
+            The edges of the k-bins.
+        '''
+        nbins = int(np.floor(np.log(self.kmax/self.kmin)/self.dlogk))
+        return np.geomspace(self.kmin, self.kmax, nbins + 1)
+
+    @property
+    def nmodes(self):
+        '''This function calculates the number of modes per k-bin shell. If nmodes was not provided, it is
+        extimated from the volume of each shell.
+
+        Returns
+        -------
+        numpy.ndarray
+            The number of modes per k-bin shell.
+        '''
+
+        if hasattr(self, '_nmodes'):
+            return self._nmodes
+
+        return math.nmodes(self.volume, self.kedges[:-1], self.kedges[1:])
--- thecov/tests/test_base.py
+++ thecov/tests/test_base.py
@@ -1,11 +1,98 @@
 import numpy as np
 import thecov.base
+import thecov.base as base
 import os
+
+def test_base_covariance_operations():
+    rng = np.random.default_rng(1)
+    a = rng.random((4, 4))
+    # make symmetric positive-definite for stable eigvals
+    mat = a @ a.T + np.eye(4) * 1e-3
+
+    cov = base.Covariance(mat)
+
+    assert np.allclose(cov.cov, mat)
+    # transpose property
+    assert np.allclose(cov.T.cov, mat.T)
+
+    # correlation matrix diagonals are 1
+    cor = cov.cor
+    assert np.allclose(np.diag(cor), np.ones(4))
+
+def test_multipole_covariance_stack_and_get():
+    rng = np.random.default_rng(2)
+    # small 2x2 blocks
+    b00 = rng.random((2, 2))
+    b02 = rng.random((2, 2))
+    b20 = rng.random((2, 2))
+    b22 = rng.random((2, 2))
+
+    m = base.MultipoleCovariance()
+
+    m.set_ell_cov(0, 0, b00)
+    m.set_ell_cov(0, 2, b02)
+    m.set_ell_cov(2, 0, b20)
+    m.set_ell_cov(2, 2, b22)
+
+    # verify that the stacked matrix contains the blocks from get_ell_cov
+    full = m.cov
+    ells1, ells2 = m.ells
+
+    # compute block sizes by inspecting diagonal blocks when present
+    sizes1 = [m.get_ell_cov(l1, l1).cov.shape[0] for l1 in ells1]
+    sizes2 = [m.get_ell_cov(l2, l2).cov.shape[1] for l2 in ells2]
+
+    rstart = 0
+    for i, l1 in enumerate(ells1):
+        rend = rstart + sizes1[i]
+        cstart = 0
+        for j, l2 in enumerate(ells2):
+            cend = cstart + sizes2[j]
+            block = full[rstart:rend, cstart:cend]
+            expected_block = m.get_ell_cov(l1, l2).cov
+            assert np.allclose(block, expected_block)
+            cstart = cend
+        rstart = rend
+
+def test_multipole_fourier_covariance_integration():
+    rng = np.random.default_rng(3)
+    cov = base.MultipoleFourierCovariance()
+    # set k-bins: kmin=0.0, kmax=0.3, dk=0.1 -> 3 bins
+    cov.set_kbins(0.0, 0.3, 0.1)
+    kb = cov.kbins
+    assert kb == 3
+
+    # build two ells -> 0 and 2
+    b00 = rng.random((kb, kb))
+    b02 = rng.random((kb, kb))
+    b20 = rng.random((kb, kb))
+    b22 = rng.random((kb, kb))
+
+    cov.set_ell_cov(0, 0, b00)
+    cov.set_ell_cov(0, 2, b02)
+    cov.set_ell_cov(2, 0, b20)
+    cov.set_ell_cov(2, 2, b22)
+
+    full = cov.cov
+    assert full.shape == (2 * kb, 2 * kb)
+
+    ells1, ells2 = cov.ells
+    # all blocks are kb x kb
+    row_offsets = [i * kb for i in range(len(ells1) + 1)]
+    col_offsets = [i * kb for i in range(len(ells2) + 1)]
+
+    for i, l1 in enumerate(ells1):
+        for j, l2 in enumerate(ells2):
+            r0, r1 = row_offsets[i], row_offsets[i + 1]
+            c0, c1 = col_offsets[j], col_offsets[j + 1]
+            block = full[r0:r1, c0:c1]
+            expected_block = cov.get_ell_cov(l1, l2).cov
+            assert np.allclose(block, expected_block)
 
 def test_multipole_covariance_symmetrization():
     cov00, cov22, cov44, cov02, cov04, cov24 = np.random.rand(6, 100, 100)
 
-    cov = thecov.base.MultipoleCovariance()
+    cov = base.MultipoleCovariance()
 
     cov.set_ell_cov(0, 0, cov00)
     cov.set_ell_cov(2, 2, cov22)
@@ -56,8 +143,8 @@ def test_multipole_covariance_addition():
     cov1_00, cov1_22, cov1_44, cov1_02, cov1_04, cov1_24 = np.random.rand(6, 100, 100)
     cov2_00, cov2_22, cov2_44, cov2_02, cov2_04, cov2_24 = np.random.rand(6, 100, 100)
 
-    cov1 = thecov.base.MultipoleCovariance()
-    cov2 = thecov.base.MultipoleCovariance()
+    cov1 = base.MultipoleCovariance()
+    cov2 = base.MultipoleCovariance()
 
     cov1.set_ell_cov(0,0, cov1_00)
     cov1.set_ell_cov(2,2, cov1_22)
@@ -85,6 +172,7 @@ def test_multipole_covariance_addition():
     assert (addition.get_ell_cov(0,4).cov == cov1_04 + cov2_04).all()
     assert (addition.get_ell_cov(2,4).cov == cov1_24 + cov2_24).all()
 
+
 # NOTE: Commented out since we will be chaning the relavent code to handle multi-tracer covariance
 # def test_multipole_fourier_covariance_save_load_csv():
 #     cov = thecov.base.MultipoleFourierCovariance()
@@ -111,3 +199,13 @@ def test_multipole_covariance_addition():
 
 #     os.remove('test1.txt')
 #     os.remove('test2.txt')
+
+
+def test_sparse_ndarray_basic():
+    # shape_out: (2, ), shape_in: (3, ) => dense shape (2,3)
+    s = base.SparseNDArray((2,), (3,))
+    # set an element at (0, 1)
+    s[0, 1] = 5.0
+    dense = s.to_dense()
+    assert dense.shape == (2, 3)
+    assert dense[0, 1] == 5.0
