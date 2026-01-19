2cfaa574d220330cf81ec11cf066f74cb5130458
Otavio Alonso Freire Alves <alonso.otavio@gmail.com>
2026-01-19 00:55:55 -0800

    spherical mesh created. invariant to nmesh

--- thecov/geometry.py
+++ thecov/geometry.py
@@ -30,6 +30,7 @@ __all__ = ['BoxGeometry',
 
 class Geometry(base.BaseClass):
     pass
+
 class BoxGeometry(Geometry):
     '''Class that represents the geometry of a periodic cubic box.
 
@@ -234,98 +235,68 @@ class BoxGeometry(Geometry):
 
 class SurveyGeometry(Geometry, base.LinearBinning):
 
-    def __init__(self, randoms1, alpha1, randoms2=None, alpha2=None, nmesh=None, cellsize=None, boxsize=None, boxpad=2., kmax=0.02, ellmax=4, **kwargs):
+    def __init__(self, randoms, alpha, nmesh=None, cellsize=None, boxsize=None, boxpad=2., kmax=0.02, ellmax=4, **kwargs):
 
         base.LinearBinning.__init__(self)
 
         self.logger = logging.getLogger('Window')
         self.tqdm = shell_tqdm
 
-        self._alpha1 = alpha1
-        self._alpha2 = alpha1 if alpha2 is None else alpha2
+        self._alpha = alpha
 
         self._kmax = kmax
         self._ellmax = ellmax
 
-        self._randoms = [randoms1]
-        self._mesh = []
+        self._randoms = randoms
 
-        if randoms2 is not None:
-            self._randoms.append(randoms2)
+        if not isinstance(randoms, mockfactory.Catalog):
+            randoms = mockfactory.Catalog(randoms)
 
-        largest_boxsize = 0
-        largest_nmesh = 0
+        # Check if the randoms have weights, otherwise set them to 1
+        if 'WEIGHT' not in randoms:
+            self.logger.warning(f'WEIGHT column not found in randoms. Setting it to 1.')
+            self.randoms['WEIGHT'] = np.ones(self.randoms.size, dtype='f8')
 
-        for randoms, alpha in zip(self._randoms, [self._alpha1, self._alpha2]):
-            if not isinstance(randoms, mockfactory.Catalog):
-                randoms = mockfactory.Catalog(randoms)
+        # Check if the randoms have a number density column, otherwise estimate it using RedshiftDensityInterpolator
+        if 'NZ' not in randoms:
+            self.logger.warning('NZ column not found in randoms. Estimating it with RedshiftDensityInterpolator.')
+            import healpy as hp
+            nside = 512
+            distance = np.sqrt(np.sum(randoms['POSITION']**2, axis=-1))
+            xyz = randoms['POSITION'] / distance[:, None]
+            hpixel = hp.vec2pix(nside, *xyz.T)
+            unique_hpixels = np.unique(hpixel)
+            fsky = len(unique_hpixels) / hp.nside2npix(nside)
+            self.logger.info(f'fsky estimated from randoms: {fsky:.3f}')
+            nbar = mockfactory.RedshiftDensityInterpolator(z=distance, fsky=fsky)
+            self.randoms['NZ'] = alpha * nbar(distance)
 
-            # Check if the randoms have weights, otherwise set them to 1
-            for name in ['WEIGHT', 'WEIGHT_FKP']:
-                if name not in randoms:
-                    self.logger.warning(f'{name} column not found in randoms. Setting it to 1.')
-                    self.randoms[name] = np.ones(self.randoms.size, dtype='f8')
+        # Check if the randoms have nmesh and cellsize, otherwise set them using the kmax parameter
+        if nmesh is None and cellsize is None:
+            # Pick value that will give at least k_mask = kmax_window in the FFTs
+            cellsize = np.pi / kmax / (1. + 1e-9)
 
-            # Check if the randoms have a number density column, otherwise estimate it using RedshiftDensityInterpolator
-            if 'NZ' not in randoms:
-                self.logger.warning('NZ column not found in randoms. Estimating it with RedshiftDensityInterpolator.')
-                import healpy as hp
-                nside = 512
-                distance = np.sqrt(np.sum(randoms['POSITION']**2, axis=-1))
-                xyz = randoms['POSITION'] / distance[:, None]
-                hpixel = hp.vec2pix(nside, *xyz.T)
-                unique_hpixels = np.unique(hpixel)
-                fsky = len(unique_hpixels) / hp.nside2npix(nside)
-                self.logger.info(f'fsky estimated from randoms: {fsky:.3f}')
-                nbar = mockfactory.RedshiftDensityInterpolator(z=distance, fsky=fsky)
-                self.randoms['NZ'] = alpha * nbar(distance)
-
-            # Check if the randoms have nmesh and cellsize, otherwise set them using the kmax parameter
-            if nmesh is None and cellsize is None:
-                # Pick value that will give at least k_mask = kmax_window in the FFTs
-                cellsize = np.pi / kmax / (1. + 1e-9)
-
-            self._mesh.append(CatalogMesh(
-                    data_positions=randoms['POSITION'],
-                    data_weights=randoms['WEIGHT'],
-                    position_type='pos',
-                    nmesh=nmesh,
-                    cellsize=cellsize,
-                    boxsize=boxsize,
-                    boxpad=boxpad,
-                    dtype='c16',
-                    **{'interlacing': 3, 'resampler': 'tsc', **kwargs}
-                ))
-            
-            if self._mesh[-1].boxsize[0] > largest_boxsize:
-                largest_boxsize = self._mesh[-1].boxsize[0]
-
-            if self._mesh[-1].nmesh[0] > largest_nmesh:
-                largest_nmesh = self._mesh[-1].nmesh[0]
-
-        # If there are multiple windows, set the box size and nmesh to the largest values
-        if len(self._mesh) > 1:
-            for mesh in self._mesh:
-                mesh._set_box(nmesh=largest_nmesh, boxsize=largest_boxsize, wrap=False)
-
-        # Log the box size and nmesh
+        self._mesh = CatalogMesh(data_positions=self._randoms['POSITION'], data_weights=self._randoms['WEIGHT']*alpha,
+                                 position_type='pos', nmesh=nmesh, cellsize=cellsize, boxsize=boxsize, boxpad=boxpad, dtype='c16',
+                                 **{'interlacing': 3, 'resampler': 'tsc', **kwargs})
         
-        self.boxsize = self._mesh[0].boxsize[0]
-        self.nmesh = self._mesh[0].nmesh[0]
-        self.logger.info(f'Using box size {self.boxsize}, box center {self._mesh[0].boxcenter} and nmesh {self.nmesh}.')
+        self.logger.info(f'Using box size {self._mesh.boxsize}, box center {self._mesh.boxcenter} and nmesh {self._mesh.nmesh}.')
+        self.boxsize = self._mesh.boxsize[0]
+        self.nmesh = self._mesh.nmesh[0]
+        assert np.allclose(self._mesh.boxsize, self.boxsize) and np.all(self._mesh.nmesh == self.nmesh)
 
-        self.logger.info(f'Fundamental wavenumber of window meshes = {self.kfun}.')
-        self.logger.info(f'Nyquist wavenumber of window meshes = {self.knyquist}.')
+        self.logger.info(f'Fundamental wavenumber of window FFTs = {self.kfun}.')
+        self.logger.info(f'Nyquist wavenumber of window FFTs = {self.knyquist}.')
 
         if kmax is not None and self.knyquist < kmax:
             self.logger.warning(f'Nyquist wavelength {self.knyquist} smaller than required window kmax = {kmax}.')
 
-        self.logger.info(f'Average of {self._mesh[0].data_size / self.nmesh**3} objects per voxel.')
+        self.logger.info(f'Average of {self._mesh.data_size / self.nmesh**3} objects per voxel.')
 
     def __getstate__(self):
         state = self.__dict__.copy()
         for key in ['logger', 'tqdm', '_randoms', '_mesh', '_resume_file']:
-            del state[key]
+            state.pop(key, None)
         return state
     
     def __setstate__(self, state):
@@ -334,22 +305,15 @@ class SurveyGeometry(Geometry, base.LinearBinning):
     @property
     def knyquist(self):
         return np.pi * self.nmesh / self.boxsize
+    
     @property
     def kfun(self):
         return 2 * np.pi / self.boxsize
     
     @property
-    def alpha1(self):
-        return self._alpha1
-    
-    @property
-    def alpha2(self):
-        return self._alpha2
-    
-    @property
     def alpha(self):
-        return self.alpha1
-
+        return self._alpha
+    
     @property
     def ikgrid(self):
         """Grid of wavenumber indices."""
@@ -360,23 +324,22 @@ class SurveyGeometry(Geometry, base.LinearBinning):
             ikgrid.append(iik)
         return ikgrid
     
-    def I(self, nbar_power, fkp_power):
-        return (self._randoms[0]['NZ']**(nbar_power-1) * \
-                self._randoms[0]['WEIGHT_FKP']**fkp_power * \
-                self._randoms[0]['WEIGHT'] * \
-                self.alpha1).sum().tolist()
-    
+    def I(self, nbar_power, weight_power):
+        return (self._randoms['NZ']**(nbar_power-1) * \
+                self._randoms['WEIGHT']**(weight_power) * \
+                self.alpha).sum().tolist()
+
     @staticmethod
-    def _shotnoise_mesh(mesh, randoms, alpha):
+    def _shotnoise_mesh(mesh, randoms):
         """Compute the shotnoise mesh S_AB = nbar * fkp^2."""
 
         return mesh.clone(
             data_positions=randoms['POSITION'],
-            data_weights=randoms['WEIGHT_FKP']**2 * randoms[f'WEIGHT'] * alpha,
+            data_weights=randoms['WEIGHT']**2,
             position_type='pos',
         ).to_mesh(compensate=True)
 
-    def mesh(self, ell, m, shotnoise=False, fourier=False, threshold=None):
+    def mesh(self, ell, m, threshold=None):
         """Compute the product of meshes and multiply by real Ylm evaluated at the same coordinates.
 
         Parameters
@@ -405,29 +368,12 @@ class SurveyGeometry(Geometry, base.LinearBinning):
         Ylm = math.get_real_Ylm(ell, m)
 
         # Initialize the result mesh
-        if shotnoise:
-            result = SurveyGeometry._shotnoise_mesh(self._mesh[0], self.randoms[0], self.alpha1)
-        else:
-            result = self._mesh[0].copy().to_mesh(compensate=True)
+        result = self._mesh.copy().to_mesh(compensate=True)
 
-        
-        if len(self._randoms) > 1 and not shotnoise:
-            result *= self._mesh[1].to_mesh(compensate=True)
+        result.apply(lambda x,W: W*np.conj(W) * Ylm(x[0], x[1], x[2]), kind='relative')
 
-        # Iterate over slabs to save memory
-        for slab in range(result.shape[0]):
-            # Multiply by other mesh (or by itself) to obtain W_AB
-            if len(self._randoms) == 1 or shotnoise:
-                result[slab, ...] *= result[slab, ...]
-
-            # Multiply by real Ylm evaluated at the same coordinates
-            result[slab, ...] *= Ylm(result.x[0][slab], result.x[1][0], result.x[2][0])
-
-            if fourier:
-                # pmesh fft convention is F(k) = 1/N^3 \sum_{r} e^{-ikr} F(r); let us correct it here
-                result[slab, ...] *= self.nmesh**3
-
-        result = result.value if not fourier else result.r2c().value
+        # pmesh fft convention is F(k) = 1/N^3 \sum_{r} e^{-ikr} F(r); let us correct it here
+        result = result.r2c().value * self.nmesh**3
 
         if threshold is not None:
             # Convert the result to a sparse array to save memory
@@ -634,7 +580,7 @@ class SurveyGeometry(Geometry, base.LinearBinning):
 
         # Returns an array with [2*delta_k_max+1,15,6] dimensions.
         #    The first dim corresponds to the k-bin of k2
-        #    (only 3 bins on each side of diagonal are included as the Gaussian covariance drops quickly away from diagonal)
+        #    (only 3 bins on each side of the diagonal are calculated as the Gaussian covariance drops quickly away from diagonal)
 
         #    The second dim corresponds to elements to be multiplied by various power spectrum multipoles
         #    to obtain the final covariance (see function 'Wij' below)
