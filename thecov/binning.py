Fixes to k-binning, normalization factors, and more mixed term code developement

thecov/binning.py
@@ -9,7 +9,7 @@ class FourierBinning:
         self.dk = None
         self._nmodes = None
 
-    def set_kbins(self, kmin:float, kmax:float, dk:float, nmodes=None):
+    def set_kbins(self, kmin:float, kmax:float, dk:float, kbins:int=None, nmodes=None):
         '''This function defines the k-bins.
 
         Parameters
@@ -20,27 +20,18 @@ class FourierBinning:
             The maximum value of the wavenumber k.
         dk: float
             The (log) spacing between k-bins.
+        kbins: int, optional
+            The number of k-bins.
         nmodes: numpy.ndarray, optional
             The number of modes to be used in the calculation. It is an optional parameter.
             If omitted, it is calculated from the volume of spherical shells.
         '''
 
-        self.dk = dk
         self.kmax = kmax
         self.kmin = kmin
+        self.dk = dk
+        self._kbins = kbins
         self._nmodes = nmodes
-
-    @property
-    def kbins(self):
-        '''Returns the total number of k-bins.
-
-        Returns
-        -------
-        int
-            The total number of k-bins.
-        '''
-
-        return len(self.kmid)
     
     @property
     def is_kbins_set(self):
@@ -51,7 +42,7 @@ class FourierBinning:
             bool, True if k-bins were defined, False otherwise.
         '''
         return None not in (self.dk, self.kmin, self.kmax)
-    
+
     @property
     def kavg(self):
         '''
@@ -111,11 +102,22 @@ class LinearBinning(FourierBinning):
         The spacing between k-bins.
     '''
 
-    def __init__(self, kmin:float=None, kmax:float=None, dk:float=None, num_kbins:int=None) -> None:
+    def __init__(self, kmin:float=None, kmax:float=None, dk:float=None, kbins:int=None) -> None:
         super().__init__()
-        if dk == None and num_kbins is not None:
-            dk = (kmax - kmin) / num_kbins
-        self.set_kbins(kmin, kmax, dk)
+        if dk is None and kbins is not None:
+            dk = (kmax - kmin) / kbins
+        self.set_kbins(kmin, kmax, dk, kbins)
+
+    @property
+    def kbins(self):
+        '''The number of k-bins. If not set, it is calculated from kmin, kmax and dk.
+
+        Returns
+        -------
+        int
+            The number of k-bins.
+        '''
+        return round((self.kmax - self.kmin) / self.dk)
 
     @property
     def kmid(self):
@@ -127,7 +129,7 @@ class LinearBinning(FourierBinning):
         numpy.ndarray
             The midpoints of the k-bins.
         '''
-        return np.arange(self.kmin + self.dk/2, self.kmax + self.dk/2, self.dk)
+        return np.linspace(self.kmin + self.dk/2, self.kmax - self.dk/2, self.kbins)
 
     @property
     def kedges(self):
@@ -139,8 +141,9 @@ class LinearBinning(FourierBinning):
         numpy.ndarray
             The edges of the k-bins.
         '''
-
-        return np.arange(self.kmin, self.kmax + self.dk/2, self.dk)
+        if self.kbins is None:
+            self.kbins = int((self.kmax - self.kmin) / self.dk)
+        return np.linspace(self.kmin, self.kmax, self.kbins + 1)
 
     @property
     def nmodes(self):
@@ -172,11 +175,22 @@ class LogBinning(FourierBinning):
         The logarithmic spacing between k-bins.
     '''
 
-    def __init__(self, kmin:float=None, kmax:float=None, dk:float=None, num_kbins:int=None) -> None:
+    def __init__(self, kmin:float=None, kmax:float=None, dk:float=None, kbins:int=None) -> None:
         super().__init__()
-        if dk == None and num_kbins is not None:
-            dk = (np.log(kmax) - np.log(kmin)) / num_kbins
-        self.set_kbins(kmin, kmax, dk)
+        if kbins is not None and dk is None:
+            dk = np.log(kmax/kmin) / kbins
+        self.set_kbins(kmin, kmax, dk, kbins)
+
+    @property
+    def kbins(self):
+        '''The number of k-bins. If not set, it is calculated from kmin, kmax and dk.
+
+        Returns
+        -------
+        int
+            The number of k-bins.
+        '''
+        return int(np.floor(np.log(self.kmax/self.kmin)/self.dk))
 
     @property
     def kmid(self):
@@ -188,7 +202,8 @@ class LogBinning(FourierBinning):
         numpy.ndarray
             The midpoints of the k-bins.
         '''
-        return np.sqrt(self.kedges[:-1]*self.kedges[1:])
+        return np.exp(np.linspace(np.log(self.kmin) + self.dk/2, np.log(self.kmax) - self.dk/2, self.kbins))
+        #return np.sqrt(self.kedges[:-1]*self.kedges[1:])
 
     @property
     def kedges(self):
@@ -200,8 +215,7 @@ class LogBinning(FourierBinning):
         numpy.ndarray
             The edges of the k-bins.
         '''
-        nbins = int(np.floor(np.log(self.kmax/self.kmin)/self.dk))
-        return np.geomspace(self.kmin, self.kmax, nbins + 1)
+        return np.geomspace(self.kmin, self.kmax, self.kbins + 1)
 
     @property
     def nmodes(self):

thecov/covariance.py
@@ -509,8 +509,8 @@ class GaussianCovariance(PowerSpectrumCovariance):
         P_AC = P_AC.reshape(1, 1, 1, l4, 1, nk2)   # align with W's l and y axes
 
         # cov has shape [n_ells, n_ells, nk, nk]
-        cov = (4*np.pi)**4 * ((WinKernel_1 * P_AD * P_BC).sum(axis=(2, 3)) + \
-                              (WinKernel_2 * P_BD * P_AC).sum(axis=(2, 3)))
+        cov = (WinKernel_1 * P_AD * P_BC).sum(axis=(2, 3)) + \
+              (WinKernel_2 * P_BD * P_AC).sum(axis=(2, 3))
         # cov = (P_AD * P_BC).sum(axis=(2, 3)) + \
         #       (P_BD * P_AC).sum(axis=(2, 3))
         

thecov/geometry.py
@@ -791,12 +791,12 @@ class SurveyGeometry(base.BaseClass):
             return gaunt_coefficients
 
     @staticmethod
-    def get_mixed_gaunt_coefficients(mask_ellmax=MASK_ELL_MAX, pk_ellmax=PK_ELL_MAX, cache_dir=None):
+    def get_mixed_gaunt_coefficients(mask_ellmax=MASK_ELL_MAX, pk_ellmax=PK_ELL_MAX, cache_dir=None, term="first"):
         """Calculates all relavent Gaunt coefficients for the mixed term, or loads them from file"""
         
         if cache_dir is None:
             cache_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "cache")
-        filename = os.path.join(cache_dir, f"mixed_coefficients_{pk_ellmax:d}_{mask_ellmax:d}.npz")
+        filename = os.path.join(cache_dir, f"mixed_coefficients_{pk_ellmax:d}_{mask_ellmax:d}_{term}.npz")
 
         logger = logging.getLogger('SurveyGeometry')
 
@@ -818,7 +818,7 @@ class SurveyGeometry(base.BaseClass):
             for l1, l2, l3, m1, m2, m3 in utils.ellmiter(pk_ellmax, 3):
 
                 lb, mb = l1, m1
-                if lb <= mask_ellmax:
+                if lb <= mask_ellmax and term == "first":
                     for la in np.arange(np.abs(l2-l3), min(l2+l3, mask_ellmax)+1, 2):
                         for ma in np.arange(-la, la+1, 2):
                             value = np.float64(sympy.physics.wigner.gaunt(l2,l3,la,m2,m3,ma))
@@ -826,7 +826,7 @@ class SurveyGeometry(base.BaseClass):
                                 gaunt_coefficients[l1//2, l2//2, l3//2, m1+l1, m2+l2, m3+l3, la//2, lb//2, ma+la, mb+lb] += value
 
                 lb, mb = l2, m2
-                if lb <= mask_ellmax:
+                if lb <= mask_ellmax and term == "second":
                     for la in np.arange(np.abs(l1-l3), min(l1+l3, mask_ellmax)+1, 2):
                         for ma in np.arange(-la, la+1, 2):
                             value = np.float64(sympy.physics.wigner.gaunt(l1,l3,la,m1,m3,ma))
@@ -834,7 +834,7 @@ class SurveyGeometry(base.BaseClass):
                                 gaunt_coefficients[l1//2, l2//2, l3//2, m1+l1, m2+l2, m3+l3, la//2, lb//2, ma+la, mb+lb] += value
 
                 la, ma = l3, m3
-                if la <= mask_ellmax:
+                if la <= mask_ellmax and term == "third":
                     for lb in np.arange(np.abs(l1-l2), min(l1+l2, mask_ellmax)+1, 2):
                         for mb in np.arange(-lb, lb+1, 2):
                             
@@ -843,14 +843,15 @@ class SurveyGeometry(base.BaseClass):
                                 gaunt_coefficients[l1//2, l2//2, l3//2, m1+l1, m2+l2, m3+l3, la//2, lb//2, ma+la, mb+lb] += value
                                 
                 lb, mb = 0,0
-                for lc in np.arange(np.abs(l1-l2), min(l1+l2, mask_ellmax)+1, 2):
-                    for la in range(np.abs(lc-l3), min(lc+l3, mask_ellmax)+1, 2):
-                        for ma in np.arange(-la, la+1, 2):
-                            for mc in range(-lc, lc+1, 2):
-                                value = np.float64(sympy.physics.wigner.gaunt(l1,l2,lc,m1,m2,mc)*\
-                                                   sympy.physics.wigner.gaunt(lc,l3,la,mc,m3,ma))
-                                if value != 0:
-                                    gaunt_coefficients[l1//2, l2//2, l3//2, m1+l1, m2+l2, m3+l3, la//2, lb//2, ma+la, mb+lb] += value
+                if term == "fourth":
+                    for lc in np.arange(np.abs(l1-l2), min(l1+l2, mask_ellmax)+1, 2):
+                        for la in range(np.abs(lc-l3), min(lc+l3, mask_ellmax)+1, 2):
+                            for ma in np.arange(-la, la+1, 2):
+                                for mc in range(-lc, lc+1, 2):
+                                    value = np.float64(sympy.physics.wigner.gaunt(l1,l2,lc,m1,m2,mc)*\
+                                                    sympy.physics.wigner.gaunt(lc,l3,la,mc,m3,ma))
+                                    if value != 0:
+                                        gaunt_coefficients[l1//2, l2//2, l3//2, m1+l1, m2+l2, m3+l3, la//2, lb//2, ma+la, mb+lb] += value
                                     
             logger.info(f'Computed {gaunt_coefficients._matrix.nnz} non-zero Gaunt coefficients')
             logger.info(f'Saving mixed Gaunt coefficients to: {filename}')
@@ -904,7 +905,7 @@ class SurveyGeometry(base.BaseClass):
         self._I = {}
 
     @base.cache
-    def compute_window_matrix(self, cache_dir:str=None, kmodes_sampled=50):
+    def compute_window_matrix(self, cache_dir:str=None, kmodes_sampled=1000):
         '''Computes the window matrix to be used in the calculation of the covariance.
 
         Notes
@@ -977,13 +978,13 @@ class SurveyGeometry(base.BaseClass):
         #shape_slab = self.compute_mesh(2,2,0,0).shape
 
         survey_window = {}
-        survey_window['first_cosmic_variance'] = self.get_cosmic_variance_window(cache_dir=cache_dir, term="first")
+        survey_window['first_cosmic_variance']  = self.get_cosmic_variance_window(cache_dir=cache_dir, term="first")
         survey_window['second_cosmic_variance'] = self.get_cosmic_variance_window(cache_dir=cache_dir, term="second")
-        survey_window['first_mixed_term']      = self.get_mixed_window(cache_dir=cache_dir, term="first")
-        survey_window['second_mixed_term']     = self.get_mixed_window(cache_dir=cache_dir, term="second")
-        survey_window['third_mixed_term']      = self.get_mixed_window(cache_dir=cache_dir, term="third")
-        survey_window['fourth_mixed_term']     = self.get_mixed_window(cache_dir=cache_dir, term="fourth")
-        survey_window['shotnoise']       = self.get_shotnoise_window(cache_dir=cache_dir)
+        survey_window['first_mixed_term']       = self.get_mixed_window(cache_dir=cache_dir, term="first")
+        survey_window['second_mixed_term']      = self.get_mixed_window(cache_dir=cache_dir, term="second")
+        survey_window['third_mixed_term']       = self.get_mixed_window(cache_dir=cache_dir, term="third")
+        survey_window['fourth_mixed_term']      = self.get_mixed_window(cache_dir=cache_dir, term="fourth")
+        survey_window['shotnoise']              = self.get_shotnoise_window(cache_dir=cache_dir)
 
 
         # S_A_temp = self.get_shotnoise_window("A", cache_dir)
@@ -1003,7 +1004,10 @@ class SurveyGeometry(base.BaseClass):
             coefficients = {
                 'first_cosmic_variance':  self.get_first_cosmic_variance_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax),
                 'second_cosmic_variance': self.get_second_cosmic_variance_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax),
-                'mixed_term':             self.get_mixed_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax),
+                'first_mixed_term':       self.get_mixed_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax, "first"),
+                'second_mixed_term':      self.get_mixed_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax, "second"),
+                'third_mixed_term':       self.get_mixed_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax, "third"),
+                'fourth_mixed_term':      self.get_mixed_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax, "fourth"),
                 'shotnoise':              self.get_shotnoise_gaunt_coefficients(self.mask_ellmax, self.pk_ellmax),
             }
         else:
@@ -1048,7 +1052,7 @@ class SurveyGeometry(base.BaseClass):
                 ik1 = np.array([ik1x, ik1y, ik1z])
                 ik1_norm = np.sqrt(np.sum(ik1**2))
                 if ik1_norm == 0:
-                    continue  # Skip the zero mode to avoid division by zero
+                    ik1_norm = 1.0
                 ik1_hat = ik1 / ik1_norm
 
                 # Compute and normalize ik2 = ik1 + delta_ik
@@ -1073,27 +1077,28 @@ class SurveyGeometry(base.BaseClass):
                         [l1//2,l2//2,l3//2,l4//2,m1+l1,m2+l2,m3+l3,m4+l4].real
                     mesh = mesh.toarray().reshape(window_product['first_cosmic_variance'].shape_in) # <- [nmesh, nmesh, nmesh]
                     mesh *= Ylm_k1[l1//2][(m1+l1)//2]*Ylm_k1[l2//2][(m2+l2)//2]*Ylm_k2[l3//2][(m3+l3)//2]*Ylm_k2[l4//2][(m4+l4)//2]
-
+                    
                     window_matrix['cosmic_variance'][0, l1//2,l2//2,l3//2,l4//2,k1_bin_index,:] += \
-                       (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins]) / len(km)
+                       (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins])
                     
                     mesh = window_product['second_cosmic_variance']\
                         [l1//2,l2//2,l3//2,l4//2,m1+l1,m2+l2,m3+l3,m4+l4].real
                     mesh = mesh.toarray().reshape(window_product['second_cosmic_variance'].shape_in)
-                    mesh *= Ylm_k1[l1//2][(m1+l1)//2]*Ylm_k1[l2//2][(m2+l2)//2]*Ylm_k2[l3//2][(m3+l3)//2]*Ylm_k2[l4//2][(m4+l4)//2]
+                    mesh *= Ylm_k1[l1//2][(m1+l1)//2]*Ylm_k2[l2//2][(m2+l2)//2]*Ylm_k2[l3//2][(m3+l3)//2]*Ylm_k1[l4//2][(m4+l4)//2]
 
                     window_matrix['cosmic_variance'][1, l1//2,l2//2,l3//2,l4//2,k1_bin_index,:] += \
-                        (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins]) / len(km)
+                        (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins])
 
                 # Mixed Term
                 for l1, l2, l3, m1, m2, m3 in utils.ellmiter(self.pk_ellmax, 3):
-                    mesh = window_product['first_mixed_term']\
-                        [l1//2,l2//2,l3//2,m1+l1,m2+l2,m3+l3].real
-                    mesh = mesh.toarray().reshape(window_product['first_mixed_term'].shape_in)
-                    mesh *= Ylm_k1[l1//2][(m1+l1)//2]*Ylm_k2[l2//2][(m2+l2)//2]*Ylm_k2[l3//2][(m3+l3)//2]
+                    for term in ["first_mixed_term", "second_mixed_term", "third_mixed_term", "fourth_mixed_term"]:
+                        mesh = window_product[term]\
+                            [l1//2,l2//2,l3//2,m1+l1,m2+l2,m3+l3].real
+                        mesh = mesh.toarray().reshape(window_product[term].shape_in)
+                        mesh *= Ylm_k1[l1//2][(m1+l1)//2]*Ylm_k2[l2//2][(m2+l2)//2]*Ylm_k2[l3//2][(m3+l3)//2]
 
-                    window_matrix['mixed_term'][0, l1//2,l2//2,l3//2,k1_bin_index,:] += \
-                        (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins]) / len(km)
+                        window_matrix[term][0, l1//2,l2//2,l3//2,k1_bin_index,:] += \
+                            (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins])
                 
                 # Shotnoise Term
                 for l1, l2, m1, m2 in utils.ellmiter(self.pk_ellmax, 2):
@@ -1103,7 +1108,7 @@ class SurveyGeometry(base.BaseClass):
                     mesh *=Ylm_k1[l1//2][(m1+l1)//2]*Ylm_k2[l2//2][(m2+l2)//2]
 
                     window_matrix['shotnoise'][l1//2,l2//2,k1_bin_index,:] += \
-                        (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins]) / len(km)
+                        (np.bincount(k2_bin_index.ravel(), weights=mesh.ravel(), minlength=self.k_binning.kbins)[:self.k_binning.kbins])
 
             # for key in window_matrix.keys():
             #     window_matrix[key] /= len(km)
@@ -1121,13 +1126,13 @@ class SurveyGeometry(base.BaseClass):
         # alpha and I22 factors are handled in covariance.py
         for k1 in range(self.k_binning.kbins):
             self.window_matrix['cosmic_variance'][:,:,:,:,:,k1,:] *= \
-                (4*np.pi)**2 / Nmodes[None,None,None,None,None,k1]
+                (4*np.pi)**4 / (len(kmodes[k1]) * Nmodes[None,None,None,None,None,k1])
 
             self.window_matrix['mixed_term'][:,:,:,:,k1,:] *= \
-                (4*np.pi)**2 / Nmodes[None,None,None,k1]
+                (4*np.pi)**2 / (len(kmodes[k1]) * Nmodes[None,None,None,k1])
             
             self.window_matrix['shotnoise'][:,:,k1,:] *= \
-                (4*np.pi)**2 / Nmodes[None,None,k1]
+                (4*np.pi)**2 / (len(kmodes[k1]) * Nmodes[None,None,k1])
         
         if self.rank == 0:
             self.logger.info('Window matrix computation completed successfully!')

thecov/math.py
@@ -169,7 +169,8 @@ def sample_kmodes(k_binning:binning.FourierBinning, boxsize:float, max_modes=100
             cube_weights = np.ones(len(cube_modes)) / len(cube_modes)
 
             # Uses spherical shell approximation from k = k_shell to kmax
-            kedges_shell = np.arange(k_shell, k_binning.kmax + k_binning.dk/2, k_binning.dk)
+            num_shell_bins = int(np.ceil((k_binning.kmax - k_shell) / k_binning.dk))
+            kedges_shell = np.linspace(k_shell, k_binning.kmax, num_shell_bins + 1)
             shell_nmodes = nmodes(boxsize**3, kedges_shell[:-1], kedges_shell[1:])
 
             shell_modes = [np.array([sample_from_shell(kmin / kfun, kmax / kfun) for _ in range(

thecov/tests/test_math.py
@@ -10,21 +10,31 @@ import pytest
 ])
 def test_sample_kmodes(kmin, kmax, num_kbins, k_shell_approx, binning_type):
     if binning_type == "linear":
-        k_binning = binning.LinearBinning(kmin, kmax, num_kbins=num_kbins)
+        k_binning = binning.LinearBinning(kmin, kmax, kbins=num_kbins)
     elif binning_type == "log":
-        k_binning = binning.LogBinning(kmin, kmax, num_kbins=num_kbins)
+        k_binning = binning.LogBinning(kmin, kmax, kbins=num_kbins)
+
+    assert num_kbins == k_binning.kbins
 
     boxsize = 1000.0
     kmodes_sampled = 50
+    kfun = 2 * np.pi / boxsize
+
     kmodes, Nmodes, weights = math.sample_kmodes(k_binning,
                                                  boxsize=boxsize,
                                                  max_modes=kmodes_sampled,
                                                  k_shell_approx=k_shell_approx,
                                                  sample_mode="monte-carlo")
-    
+
+    kedges = k_binning.kedges / kfun
     # check that the number of modes in each bin is correct
-    for i in range(num_kbins):
+    # and that each mode's magnitude falls within the correct bin edges
+    for i in range(k_binning.kbins):
         assert kmodes[i].shape[0] == int(min(Nmodes[i], kmodes_sampled))
+        shell_magnitudes = kmodes[i][:, 3]
+        assert np.all(shell_magnitudes >= kedges[i])
+        assert np.all(shell_magnitudes < kedges[i+1])
+
     assert len(Nmodes) == num_kbins
     assert len(weights) == num_kbins
 
@@ -52,3 +62,22 @@ def test_evaluate_Ylms_matches_get_real_Ylm():
         direct = math.get_real_Ylm(l, m)(kxh, kyh, kzh)
         via_table = evaluated[l_idx][m_idx]
         assert np.allclose(via_table, np.array(direct))
+
+
+def test_k_binning_properties():
+    kmin = 0.1
+    kmax = 1.0
+    dk = (kmax - kmin) / 10
+    num_kbins = 10
+
+    linear_binning = binning.LinearBinning(kmin, kmax, dk)
+    assert linear_binning.kbins == num_kbins
+    assert np.allclose(linear_binning.kedges, np.linspace(kmin, kmax, num_kbins + 1))
+    assert np.allclose(linear_binning.kmid, np.linspace(kmin+linear_binning.dk/2, kmax-linear_binning.dk/2, num_kbins))
+
+    log_binning = binning.LogBinning(kmin, kmax, kbins=num_kbins)
+    assert log_binning.kbins == num_kbins
+    # kedges should be geometrically spaced including both endpoints
+    assert np.allclose(log_binning.kedges, np.geomspace(kmin, kmax, num_kbins + 1))
+    # kmid should be geometric midpoints (sqrt of edge products)
+    assert np.allclose(log_binning.kmid, np.sqrt(log_binning.kedges[:-1] * log_binning.kedges[1:]))