Fixes to pytests and window function off-diagonal normalization

thecov/covariance.py
@@ -149,8 +149,8 @@ class PowerSpectrumCovariance(base.MultipoleFourierCovariance):
         shotnoise = []
         if isinstance(self.geometry, geometry.SurveyGeometry):
             alphas = np.array(self.alpha)
-            for t in range(self.num_tracers):
-                shotnoise.append(self.pk_renorm * (1 + alphas[t]) * self.geometry.I(t, 1, 2)/self.geometry.I(t, 2, 2))
+            for t1 in range(self.num_tracers):
+                shotnoise.append(self.pk_renorm * (1 + alphas[t1]) * self.geometry.I(t1, t1, 1, 2)/self.geometry.I(t1, t1, 2, 2))
             return np.array(shotnoise)
         elif isinstance(self.geometry, geometry.BoxGeometry):
             return self.pk_renorm * self.geometry.shotnoise
@@ -291,12 +291,9 @@ class GaussianCovariance(PowerSpectrumCovariance):
         # terms without the power spectrum have to be multiplied by its relative normalization pk_renorm
         def func(A, B, C, D): 
             
-            I_AB = (self.geometry.I(A, 2, 2) * self.geometry.I(B, 2, 2))
-            I_CD = (self.geometry.I(C, 2, 2) * self.geometry.I(D, 2, 2)) 
-            return (1 / (I_AB * I_CD)) * \
-            (self._get_cosmic_variance_term(A, B, C, D))# + \
-             #self._get_mixed_term(A, B, C, D) + \
-             #self._get_shotnoise_term(A, B))
+            return self._get_cosmic_variance_term(A, B, C, D) + \
+                   self._get_mixed_term(A, B, C, D) + \
+                   self._get_shotnoise_term(A, B, C, D)
 
         self._set_survey_covariance(self._build_covariance_survey(func))
         eigvals = self.eigvals
@@ -498,7 +495,6 @@ class GaussianCovariance(PowerSpectrumCovariance):
         P_AC = np.array([1. / (2*ell + 1) * \
                          self.get_pk(ell, A, C, force_return=True, remove_shotnoise=True) for ell in [0,2,4]])
 
-
         cov = np.einsum('ijklxy,kx,ly->ijxy', WinKernel_1, P_AD, P_BC) + \
               np.einsum('ijklxy,kx,ly->ijxy', WinKernel_2, P_BD, P_AC)
         
@@ -532,36 +528,41 @@ class GaussianCovariance(PowerSpectrumCovariance):
 
         cov = np.zeros((3, 3, self.k_binning.kbins, self.k_binning.kbins))
         if A == D:
-            cov += (1 + self.alpha[TRACER_LABELS[A]]) / 2 * \
+            cov += (1 + self.alpha[A]) / 2 * \
             (np.einsum('ijkxy,kx->ijxy', W_mixed[0], P_BC) + \
              np.einsum('ijkxy,ky->ijxy', W_mixed[0], P_BC))
         if B == C:
-            cov += (1 + self.alpha[TRACER_LABELS[B]]) / 2 * \
+            cov += (1 + self.alpha[B]) / 2 * \
                 (np.einsum('ijkxy,kx->ijxy', W_mixed[1], P_AD) + \
                  np.einsum('ijkxy,ky->ijxy', W_mixed[1], P_AD))
         if A == C:
-            cov += (1 + self.alpha[TRACER_LABELS[A]]) / 2 * \
+            cov += (1 + self.alpha[A]) / 2 * \
                 (np.einsum('ijkxy,kx->ijxy', W_mixed[2], P_BD) + \
                  np.einsum('ijkxy,ky->ijxy', W_mixed[2], P_BD))
         if B == D:
-            cov += (1 + self.alpha[TRACER_LABELS[B]]) / 2 * \
+            cov += (1 + self.alpha[B]) / 2 * \
                 (np.einsum('ijkxy,kx->ijxy', W_mixed[3], P_AC) + \
                  np.einsum('ijkxy,ky->ijxy', W_mixed[3], P_AC))
 
         return cov
 
-    def _get_shotnoise_term(self, A:int, B:int):
+    def _get_shotnoise_term(self, A:int, B:int, C:int, D:int):
         """Calculates elements of the shotnoise Gaussian term
 
         Args:
             A (int): First tracer index
             B (int): Second tracer index
+            C (int): Third tracer index
+            D (int): Fourth tracer index
 
         Returns:
             np.ndarray: shotnoise term contribution with shape [n_ells, n_ells, nk, nk]
         """
         WinKernel = self.geometry.shotnoise_kernel(A,B)
-        return (1 + self.alpha[TRACER_LABELS[A]]) * (1 + self.alpha[TRACER_LABELS[B]]) * WinKernel
+        if A == D and B == C:
+            return (1 + self.alpha[A]) * (1 + self.alpha[B]) * WinKernel
+        else:
+            return np.zeros((3, 3, self.k_binning.kbins, self.k_binning.kbins))
 
 
 # TODO: Update this to multi-tracer

thecov/tests/test_covariance.py
@@ -3,7 +3,7 @@ import pytest
 import os
 
 from mockfactory.make_survey import RandomBoxCatalog
-from thecov import covariance, geometry, binning
+from thecov import covariance, geometry
 
 def create_basic_randoms(num_tracers):
 
@@ -11,12 +11,9 @@ def create_basic_randoms(num_tracers):
 	boxsize = 1000.0
 
 	randoms = []
-	for t in range(4):
-		if t+1 <= num_tracers:
-			randoms.append(RandomBoxCatalog(nbar=nbar[t], boxsize=boxsize))
-			randoms[t]["POSITION"] = randoms[t]["Position"]
-		else:
-			randoms.append(None)
+	for t in range(num_tracers):
+		randoms.append(RandomBoxCatalog(nbar=nbar[t], boxsize=boxsize))
+		randoms[t]["POSITION"] = randoms[t]["Position"]
 	return randoms
 
 def test_set_galaxy_pk_multipole_stores_symmetric_keys():
@@ -24,10 +21,7 @@ def test_set_galaxy_pk_multipole_stores_symmetric_keys():
 	randoms = create_basic_randoms(num_tracers=3)
 	alpha = [0.1, 0.1, 0.1]
 	kmax = 0.05
-	g = geometry.SurveyGeometry(randoms[0], alpha[0],
-							    randoms[1], alpha[1],
-							    randoms[2], alpha[2],
-							    None, None,
+	g = geometry.SurveyGeometry(randoms, alpha,
 							    nmesh=32, boxpad=1.2,
 							    kmin=0.001, kmax=kmax, dk=0.005,
 								resume_file="test.npy")
@@ -60,10 +54,7 @@ def test_get_tracer_cov_labels(num_tracers, tracer1, tracer2, expected):
 	randoms = create_basic_randoms(num_tracers)
 	alpha = [0.1, 0.1, 0.1, 0.1]
 	kmax = 0.05
-	g = geometry.SurveyGeometry(randoms[0], alpha[0],
-							    randoms[1], alpha[1],
-							    randoms[2], alpha[2],
-							    randoms[3], alpha[3],
+	g = geometry.SurveyGeometry(randoms, alpha,
 							    nmesh=32, boxpad=1.2,
 							    kmin=0.001, kmax=kmax, dk=0.005,
 								resume_file="test.npy")
@@ -90,10 +81,7 @@ def test_shotnoise_computation_uses_geometry_I_and_alphas_and_pk_renorm():
 	randoms = create_basic_randoms(num_tracers=2)
 	alpha = [0.1, 0.1]
 	kmax = 0.05
-	g = geometry.SurveyGeometry(randoms[0], alpha[0],
-							    randoms[1], alpha[1],
-								None, None,
-							    None, None,
+	g = geometry.SurveyGeometry(randoms, alpha,
 							    nmesh=32, boxpad=1.2,
 							    kmin=0.001, kmax=kmax, dk=0.005,
 								resume_file="test.npy")
@@ -101,10 +89,10 @@ def test_shotnoise_computation_uses_geometry_I_and_alphas_and_pk_renorm():
 	cov = covariance.PowerSpectrumCovariance(geometry=g)
 
 	# compute expected shotnoise per tracer
-	alphas_array = np.array(list(g.alphas.values()))
+	alphas_array = np.array(g.alphas)
 	expected = []
 	for t in range(g.num_tracers):
-		expected.append(cov.pk_renorm * (1 + alphas_array[t]) * g.I(TRACER_LABELS[t], 1, 2) / g.I(TRACER_LABELS[t], 2, 2))
+		expected.append(cov.pk_renorm * (1 + alphas_array[t]) * g.I(t, t, 1, 2) / g.I(t, t, 2, 2))
 	expected = np.array(expected)
 
 	sn = cov.shotnoise
@@ -116,10 +104,7 @@ def test_load_npy_file_raises_on_wrong_dimensions(tmp_path):
 	randoms = create_basic_randoms(num_tracers=2)
 	alpha = [0.1, 0.1, 0.1]
 	kmax = 0.05
-	g = geometry.SurveyGeometry(randoms[0], alpha[0],
-							    randoms[1], alpha[1],
-							    randoms[2], alpha[2],
-							    None, None,
+	g = geometry.SurveyGeometry(randoms, alpha,
 							    nmesh=32, boxpad=1.2,
 							    kmin=0.001, kmax=kmax, dk=0.005,
 								resume_file="test.npy")