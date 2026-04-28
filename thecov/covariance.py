Fixed incorrect normalization for shotnoise estimation

thecov/covariance.py
@@ -154,7 +154,7 @@ class PowerSpectrumCovariance(base.MultipoleFourierCovariance):
         if isinstance(self.geometry, geometry.SurveyGeometry):
             alphas = np.array(self.alpha)
             for t1 in range(self.num_tracers):
-                shotnoise.append(self.pk_renorm * (1 + alphas[t1]) * self.geometry.I(t1, t1, 1, 1, 0, 0)/self.geometry.I(t1, t1, 1, 1, 1, 1))
+                shotnoise.append(self.pk_renorm * (1 + alphas[t1]) * self.geometry.I(t1, t1, 1, 2, 0, 0)/self.geometry.I(t1, t1, 1, 1, 1, 1))
             return np.array(shotnoise)
         elif isinstance(self.geometry, geometry.BoxGeometry):
             return self.pk_renorm * self.geometry.shotnoise
@@ -434,7 +434,7 @@ class GaussianCovariance(PowerSpectrumCovariance):
             if set_shotnoise:
                 self.set_shotnoise(shotnoise=pypower.shotnoise)
             else:
-                self.pk_renorm = self.geometry.I(tracer1, tracer2, 2,2) / pypower.wnorm * naverage
+                self.pk_renorm = self.geometry.I(tracer1, tracer2, 1,1,1,1) / pypower.wnorm * naverage
                 self.logger.info(
                     f'Renormalizing by a factor of {self.pk_renorm:.2f} to match pypower power spectrum normalization.')
 

thecov/geometry.py
@@ -293,27 +293,8 @@ class SurveyWindow(base.BaseClass):
         unit_positions = self.mesh1.data_positions / np.sqrt(np.sum(self.mesh1.data_positions**2, axis=-1))[:, None]
         self.comm.Barrier()
 
-        # FRIDAY 04/17 NOTE
-        # If you compare mesh vs random quantities, they will differ by a factor of 
-        # random = mesh * n(z). This approach clearly worked in the past with single-tracer,
-        # but now I am multiplying 2 meshes together, so maybe that's not correct anymore?
-
         # W_A or S_A
-        # NOTE: factor of alpha is needed here because, even if the randoms are
-        # corrected to match the data n(z), they are still oversampled by a factor of 1/alpha.
-        # Since the mesh is painted with the randoms weights, it is effectively painted with nbar*alpha 
-        # instead of nbar, so we need to multiply by alpha here to get the correct normalization.
-        # if apply_nz and apply_alpha:
-        #     weights = mesh_to_clone.data_weights * self.nz1 * self.alpha1
-        # elif apply_nz and not apply_alpha:
-        #     weights = mesh_to_clone.data_weights * self.nz1
-        # elif not apply_nz and apply_alpha:
-        #     weights = mesh_to_clone.data_weights * self.alpha1
-        # else:
-        #     weights = mesh_to_clone.data_weights
         cell_vol = ((self.mesh1.boxsize[0] / self.mesh1.nmesh[0])**3)
-        #print(cell_vol)
-
         weights = self.nz1 ** (nbar_power_1 - 1) * (self.mesh1.data_weights ** fkp_power_1)
         result = self.mesh1.clone(
                 data_positions=self.mesh1.data_positions,
@@ -528,7 +509,7 @@ class SurveyGeometry(base.BaseClass):
         This distinction is to ensure any mesh
         """
         #self.I_LABELS = ['12', '22', '10', '24', '14', '34', '44', '32']
-        self.I_LABELS = ["1100", "1111"]
+        self.I_LABELS = ["1200", "1111"]
         self._I = np.full((len(self.I_LABELS), self.num_tracers, self.num_tracers), 1.0)
 
         for t1, t2 in itt.product(range(self.num_tracers), repeat=2):
@@ -1142,7 +1123,7 @@ class SurveyGeometry(base.BaseClass):
             for ik1x, ik1y, ik1z, ik1r in chunks[self.rank]:
 
                 ik1 = np.array([ik1x, ik1y, ik1z])
-                if ik1r == 0: ik1_hat = ik1
+                if ik1r == 0: ik1_hat = np.array([1, 0, 0])
                 else:         ik1_hat = ik1 / ik1r
 
                 # Compute and normalize ik2 = ik1 + delta_ik

thecov/tests/test_covariance.py
@@ -93,7 +93,7 @@ def test_shotnoise_computation_uses_geometry_I_and_alphas_and_pk_renorm():
 	alphas_array = np.array(g.alphas)
 	expected = []
 	for t in range(g.num_tracers):
-		expected.append(cov.pk_renorm * (1 + alphas_array[t]) * g.I(t, t, 1, 2) / g.I(t, t, 2, 2))
+		expected.append(cov.pk_renorm * (1 + alphas_array[t]) * g.I(t, t, 1, 2) / g.I(t, t, 1, 1, 1, 1))
 	expected = np.array(expected)
 
 	sn = cov.shotnoise